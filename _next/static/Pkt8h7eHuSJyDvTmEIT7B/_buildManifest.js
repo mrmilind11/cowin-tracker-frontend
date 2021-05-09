9d3a9f0c1795123ccbbf704d358ddb19d60aace3
Milind Sharma <milind.sharma@searce.com>
2021-05-09 20:51:59 +0530

    Updates

---
+++ _next/static/Pkt8h7eHuSJyDvTmEIT7B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/350-e98f57d452fbcb2673fc.js","static/chunks/pages/index-1e0aaeb6245c3b3925f9.js"],"/_error":["static/chunks/pages/_error-e34024a730ba42383b62.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/SfVfNDjcvPcPVp5QruhOm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/350-e98f57d452fbcb2673fc.js","static/chunks/pages/index-50daf08b31a6c4006468.js"],"/_error":["static/chunks/pages/_error-e34024a730ba42383b62.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
